b5c8ad8fcccafed25cad9d2de9fc09d1f976d0d3
rohithsingh1 <rohithsinghthakur3@gmail.com>
2026-06-18 16:29:21 +0530

    feat: learning vue.js 2x version

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+Vue.component('greeting', {
+    template: `<p>Hey there, I am {{name}}. <button v-on:click="changeName">Change Name</button></p>`,
+    data: function () {
+        return {
+            name: 'Rohith'
+        }
+    },
+    methods: {
+        changeName: function () {
+            this.name = 'Codevolution'
+        }
+    }
+})
+
 new Vue({
     el: "#vue-app",
     data: {
@@ -11,23 +25,31 @@ new Vue({
         InputAge: '',
         a: 0,
         b: 0,
+        isAvailable: false,
+        isNearby: false,
+        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+        ninjas: [
+            { name: 'Ryu', age: 25 },
+            { name: 'Yoshi', age: 35 },
+            { name: 'Ken', age: 55 }
+        ]
     },
     methods: {
         greet: function (time) {
             return `Good ${time} ${this.name}`
         },
         AddAge: function (val) {
-            this.age=this.age+val
+            this.age = this.age + val
             return this.age
         },
         SubtractAge: function (val) {
-            this.age=this.age-val
+            this.age = this.age - val
             return this.age
         },
         UpdateXY: function (event) {
             console.log(event)
-            this.x=event.offsetX;
-            this.y=event.offsetY;
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         },
         clickAnchorTag: function () {
             alert('u clicked the tag')
@@ -37,16 +59,52 @@ new Vue({
         },
         logAge: function () {
             console.log('you entered your age');
-        }
+        },
+        DynamicClass: function () {
+            this.isAvailable = !this.isAvailable;
+            this.isNearby = !this.isNearby;
+        },
     },
     computed: {
         addToA: function () {
             console.log('addToA');
-            return this.a+this.age;
+            return this.a + this.age;
         },
         addToB: function () {
             console.log('addToB');
-            return this.b+this.age;
+            return this.b + this.age;
+        },
+        computedClasses: function () {
+            return {
+                available: this.isAvailable,
+                nearby: this.isNearby
+            }
         }
     }
+})
+
+
+var one = new Vue({
+    el: "#vue-app-one",
+    data: {
+        title: 'Vue App One',
+        name: ''
+    },
+    methods: {
+        submitName: function () {
+            this.name = this.$refs.nameInput.value;
+        }
+    }
+})
+
+var two = new Vue({
+    el: "#vue-app-two",
+    data: {
+        title: 'Vue App Two'
+    },
+    methods: {
+        changeOneTitle: function () {
+            one.title = "Title Changed"
+        }
+    },
 })
